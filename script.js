22950bd85cc1b45f0c47bbc9b38eb558c0023a27
ImranParthib <imranparthib13@gmail.com>
2024-06-19 20:22:23 +0600

    💄 refactor: Update responsive styles for navbar, logo, and sections

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var header = document.getElementById("container");
 var sticky = header.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset > sticky) {
+  if (window.scrollY > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
